64f578883b97b7d478e34cc36908af11fc46932e
Saad Beidouri <saad.beidouri@etu.unice.fr>
2023-12-23 11:09:14 +0100

    implement f function

--- include/encryption.h
+++ include/encryption.h
@@ -6,8 +6,8 @@ typedef struct {
     char signature[1024];
 } data;
 
-void encrypt(char *input, char *output, char *key);
-void decrypt(char *input, char *output, char *key);
+void encrypt(uint64_t input, uint64_t *output, uint64_t key[3]);
+void decrypt(uint64_t input, uint64_t *output, uint64_t key[3]);
 void encrypt_file(char *input, char *output, char *key);
 void decrypt_file(char *input, char *output, char *key);
 void sign(char *input, char *output, char *key);
--- src/encryption.c
+++ src/encryption.c
@@ -64,6 +64,47 @@ int FP[] = {
     33, 1, 41, 9, 49, 17, 57, 25
 };
 
+// Placeholder S-box (substitution box)
+int S[8][4][16] = {
+    {
+        {14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7}, // S1
+        {0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8},
+        {4, 1, 13, 8, 6, 2, 11, 15, 12, 9, 7, 3, 10 ,5, 14, 0},
+        {15, 12, 8, 2, 4, 9, 1, 7 ,5 ,11, 3, 14, 10, 0, 6, 13}
+    },
+    {
+        {15 ,1 ,8 ,14 ,6 ,11 ,3 ,4 ,9 ,7 ,2 ,13 ,12 ,0 ,5 ,10}, // S2
+        {3 ,13 ,4 ,7 ,15 ,2 ,8 ,14 ,12 ,0 ,1 ,10 ,6 ,9 ,11 ,5},
+        {0 ,14 ,7 ,11 ,10 ,4 ,13 ,1 ,5 ,8 ,12 ,6 ,9 ,3 ,2 ,15},
+        {13 ,8 ,10 ,1 ,3 ,15 ,4 ,2 ,11 ,6 ,7 ,12 ,0 ,5 ,14 ,9}
+    },
+    {
+        {10 ,0 ,9 ,14 ,6 ,3 ,15 ,5 ,1 ,13 ,12 ,7 ,11 ,4 ,2 ,8}, // S3
+        {13 ,7 ,0 ,9 ,3 ,4 ,6 ,10 ,2 ,8 ,5 ,14 ,12 ,11 ,15 ,1},
+        {13 ,6 ,4 ,9 ,8 ,15 ,3 ,0 ,11 ,1 ,2 ,12 ,5 ,10 ,14 ,7},
+        {1 ,10 ,13 ,0 ,6 ,9 ,8 ,7 ,4 ,15 ,14 ,3 ,11 ,5 ,2 ,12}
+    },
+};
+// Permutation table for final permutation step
+int P[32] = {
+    16, 7, 20, 21, 29, 12, 28, 17,
+    1, 15, 23 ,26 ,5 ,18 ,31 ,10,
+    2 ,8 ,24 ,14 ,32 ,27 ,3 ,9,
+    19 ,13 ,30 ,6 ,22 ,11 ,4 ,25
+};
+
+int E[48] = {
+    32,  1,  2,  3,  4,  5,
+     4,  5,  6,  7,  8,  9,
+     8,  9, 10, 11, 12, 13,
+    12, 13, 14, 15, 16, 17,
+    16, 17, 18, 19, 20, 21,
+    20, 21, 22, 23, 24, 25,
+    24, 25, 26, 27, 28, 29,
+    28, 29, 30, 31, 32,  1
+};
+
+
 
 
 // Permutation function to map input using a permutation table
@@ -148,8 +189,34 @@ uint64_t desDecryptBlock(uint64_t input, uint64_t* subkeys) {
     return permute(combined, FP, 64);
 }
 
+// The Feistel function
+uint64_t f(uint64_t r, uint64_t k) {
+    // Expansion permutation
+    uint64_t expandedR = permute(r, E, 48);
 
-void encrypt(char *input, char *output, char *key){
+    // XOR with the key
+    uint64_t xored = expandedR ^ k;
+
+    // Substitution boxes
+    uint64_t substituted = 0;
+    for (int i = 0; i < 8; i++) {
+        // Get the bits we are currently processing
+        uint64_t bits = (xored >> (42 - i * 6)) & 0x3F;
+
+        // Get the row and column numbers
+        int row = ((bits & 0x20) >> 4) | (bits & 1);
+        int col = (bits >> 1) & 0xF;
+
+        // Look up the value in the S-box table
+        substituted |= (uint64_t)S[i][row][col] << (32 - 4 * (i + 1));
+    }
+
+    // Straight permutation
+    return permute(substituted, P, 32);
+}
+
+// Corrected encrypt function
+void encrypt(uint64_t input, uint64_t *output, uint64_t key[3]){
     uint64_t roundKeys1[16], roundKeys2[16], roundKeys3[16];
 
     // Generate subkeys for all three keys
@@ -160,15 +227,15 @@ void encrypt(char *input, char *output, char *key){
     // Encrypt with first key
     uint64_t temp = desEncryptBlock(input, roundKeys1);
 
-    // Decrypt with second key 
+    // Decrypt with second key (using a decryption function, not shown here)
     temp = desDecryptBlock(temp, roundKeys2);
 
     // Encrypt with third key
     *output = desEncryptBlock(temp, roundKeys3);
 }
 
-
-void decrypt(char *input, char *output, char *key){
+// Corrected decrypt function
+void decrypt(uint64_t input, uint64_t *output, uint64_t key[3]){
     uint64_t roundKeys1[16], roundKeys2[16], roundKeys3[16];
 
     // Generate subkeys for all three keys
@@ -179,7 +246,7 @@ void decrypt(char *input, char *output, char *key){
     // Decrypt with third key
     uint64_t temp = desDecryptBlock(input, roundKeys3);
 
-    // Encrypt with second key
+    // Encrypt with second key (using a decryption function, not shown here)
     temp = desEncryptBlock(temp, roundKeys2);
 
     // Decrypt with first key
@@ -198,10 +265,10 @@ int main() {
     };
 
     // Encrypt the data
-    encrypt(&input, &output, keys);
+    encrypt(input, &output, keys);
 
     // Decrypt the data
-    decrypt(&output, &decrypted, keys);
+    decrypt(output, &decrypted, keys);
 
     // Compare the decrypted data with the original input
     if (input == decrypted) {
