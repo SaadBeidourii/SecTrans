fbf7bc433f0d0bebdb19c3d51355554c29295478
Anas Chhilif <anaschhilif33@gmail.com>
2024-01-05 19:22:33 +0000

    finally added unencrypted file storing

--- src/clientsectrans.c
+++ src/clientsectrans.c
@@ -9,6 +9,15 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
+#define MAX_FILE_SIZE 10485760
+#define DOCKER_SERVER_PORT_NUMBER 3000
+
+/**
+ * Calculates the SHA-256 hash of a string.
+ *
+ * @param input The string to hash.
+ * @param outputBuffer The buffer to store the hash in.
+ */
 void sha256(const char *input, char outputBuffer[65]) {
   EVP_MD_CTX *mdctx;
   const EVP_MD *md;
@@ -29,13 +38,20 @@ void sha256(const char *input, char outputBuffer[65]) {
   EVP_DigestFinal_ex(mdctx, hash, &hash_len);
   EVP_MD_CTX_free(mdctx);
 
-  for (int i = 0; i < hash_len; i++) {
+  for (unsigned int i = 0; i < hash_len; i++) {
     sprintf(outputBuffer + (i * 2), "%02x", hash[i]);
   }
 
   outputBuffer[64] = '\0';
 }
 
+/**
+ * Returns the file name from a path.
+ *
+ * @param path The path to the file.
+ *
+ * @return The file name.
+ */
 const char *getFileName(const char *path) {
   // Find the last occurrence of the directory separator "/"
   const char *lastSlash = strrchr(path, '/');
@@ -45,41 +61,76 @@ const char *getFileName(const char *path) {
     return path;
   }
 
-  // Return the portion of the path after the last slash
+  // Re3turn the portion of the path after the last slash
   return lastSlash + 1;
 }
 
+/**
+ * Sends a file to the server.
+ *
+ * @param filePath The path to the file you want to send.
+ *
+ * @return 0 on success, 1 on failure.
+ */
 int send_file(char *filePath) {
+  char *fileContent = (char *)malloc(MAX_FILE_SIZE * sizeof(char));
   char firstMessage[1024] = "up ";
-  strncat(firstMessage, getFileName(filePath), MIN(99, strlen(filePath)));
-  sndmsg(firstMessage, 4000);
+  char hash[65];
   char buffer[1024];
   int size = 0;
+  //
   // open the file
   int fd;
   fd = open(filePath, O_RDONLY);
   if (fd <= 0) {
     fprintf(stderr, "Couldn't open %s\n", filePath);
+    free(fileContent);
     exit(1);
   }
   struct stat stat_data;
   if (fstat(fd, &stat_data) < 0) {
     fprintf(stderr, "Failed to stat %s: %s\n", filePath, strerror(errno));
+    free(fileContent);
     exit(1);
   }
   if (stat_data.st_uid == 0) {
     fprintf(stderr, "File %s is owned by root\n", filePath);
+    free(fileContent);
     exit(1);
   }
 
+  int counter = 0;
   do {
+    counter++;
     size = read(fd, buffer, 1024);
-    sndmsg(buffer, 4000);
-    write(1, buffer, size);
-    sleep(2);
+    strncat(fileContent, buffer, size);
+    // sndmsg(buffer, DOCKER_SERVER_PORT_NUMBER);
+    // write(1, buffer, size);
     memset(buffer, 0, 1024);
-  } while (size > 0);
+  } while (size > 0 && counter <= 1024);
 
+  if (size > 0) {
+    printf("maximum size is 10Mib\n");
+    free(fileContent);
+    return -1;
+  }
+
+  strncat(firstMessage, getFileName(filePath), MIN(99, strlen(filePath)));
+  strcat(firstMessage, " ");
+  sha256(fileContent, hash);
+  strcat(firstMessage, hash);
+  sndmsg(firstMessage, DOCKER_SERVER_PORT_NUMBER);
+  sprintf(buffer, "%d", (int)strlen(fileContent));
+  sndmsg(buffer, DOCKER_SERVER_PORT_NUMBER);
+  for (int i = 0; i < counter - 1; i++) {
+    memset(buffer, 0, 1024);
+    memcpy(buffer, fileContent + (i * 1024), 1024);
+    printf("%s\n", buffer);
+    sndmsg(buffer, DOCKER_SERVER_PORT_NUMBER);
+    sleep(1);
+  }
+
+  free(fileContent);
   return 0;
 }
 
--- src/serversectrans.c
+++ src/serversectrans.c
@@ -2,18 +2,102 @@
 #include "../include/server.h"
 #include <sqlite3.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
+#define MAX_FILE_SIZE 10485760
+
+void createAndWriteToFile(const char *fileName, const char *fileContent) {
+  // Define the directory where the file will be saved
+  const char *directory = "./files/";
+
+  // Concatenate the directory and file name to get the full path
+  char fullPath[256]; // Adjust the size as needed
+  snprintf(fullPath, sizeof(fullPath), "%s%s", directory, fileName);
+  printf("Full Path: %s\n", fullPath);
+
+  // Open the file in write mode
+  FILE *file = fopen(fullPath, "w");
+
+  // Check if the file is successfully opened
+  if (file == NULL) {
+    printf("Error opening file %s\n", fullPath);
+    return;
+  }
+
+  // Write the content to the file
+  fprintf(file, "%s", fileContent);
+
+  // Close the file
+  fclose(file);
+
+  printf("File %s created and written successfully.\n", fullPath);
+}
+
+// Function to split a string into two tokens based on spaces
+void splitString(const char *input, char token1[1024], char token2[1024]) {
+  // Use strtok to get the first token
+  char *firstToken = strtok((char *)input, " ");
+  if (firstToken != NULL) {
+    strcpy(token1, firstToken);
+
+    // Use strtok to get the second token
+    char *secondToken = strtok(NULL, " ");
+    if (secondToken != NULL) {
+      strcpy(token2, secondToken);
+    } else {
+      // If there's no second token, set the second token to an empty string
+      token2[0] = '\0';
+    }
+  } else {
+    // If there's no first token, set both tokens to an empty string
+    token1[0] = '\0';
+    token2[0] = '\0';
+  }
+}
+
+void recieveFile(char *fileName) {
+  printf("currently recieving file %s\n", fileName);
+  char *fileContent = (char *)malloc(MAX_FILE_SIZE * sizeof(char));
+  /*
+  for(int i = 0; i < 1024; i++) {
+    char message[1024];
+    getmsg(message);
+    strncat(fileContent, message, 1024);
+  }
+  */
+
+  char message[1024];
+  getmsg(message);
+  printf("%s\n", message);
+  printf("%d\n", atoi(message));
+  int fileSize = atoi(message);
+  for(int i = 0; (i)*1024 < fileSize; i++) {
+    getmsg(message);
+    strncat(fileContent, message, 1024);
+    printf("recieved message: %s\n", message);
+    memset(message, 0, sizeof(message));
+  }
+  printf("file recieved\n");
+
+  createAndWriteToFile(fileName, fileContent);
+}
 
 /**
  * the main function of our application server
  */
 int main() {
+  char firstMessage[2][1024];
   if (startserver(3000) == 0) {
     printf("Server started successfully!\n");
     while (1) {
       char message[1024];
       getmsg(message);
-      printf("%s\n", message);
+      //      printf("%s\n", message);
+      splitString(message, firstMessage[0], firstMessage[1]);
+      if (memcmp(firstMessage[0], "up", 3) == 0) {
+        recieveFile(firstMessage[1]);
+      }
       memset(message, 0, sizeof(message));
     }
   }
